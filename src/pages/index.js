b2e1703bb57b4c15072a69a6c731b4709c6168bb
Cédric <cedric.bartolacelli@heig-vd.ch>
2025-04-04 11:13:33 +0200

    Ajustement taille image oiseaux et ajout svg icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
 import BirdStrikePeriods from '../components/birdStrikePeriods.js';
+import Species from '../components/species.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const container = document.getElementById('daytime-container');
-    if (container) {
-        new BirdStrikePeriods(container);
+    // Initialisation de BirdStrikePeriods
+    const daytimeContainer = document.getElementById('daytime-container');
+    if (daytimeContainer) {
+        new BirdStrikePeriods(daytimeContainer);
     } else {
-        console.error('Container not found');
+        console.error('Daytime container not found');
+    }
+
+    // Initialisation du carousel des espèces
+    const speciesContainer = document.getElementById('species-container');
+    if (speciesContainer) {
+        new Species(speciesContainer);
+    } else {
+        console.error('Species container not found');
     }
 });
